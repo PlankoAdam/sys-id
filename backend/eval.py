7634d713afd28c145b81dcab046c01f0b4f00e0d
Adam Planko <adi.planko@gmail.com>
2026-05-10 11:10:15 +0200

    retrain

--- backend/eval.py
+++ backend/eval.py
@@ -2,6 +2,10 @@ from src.tftype_classifier.eval_model import eval_model as eval_tftype_classifie
 from src.tf_predictor.eval_model import eval_all as eval_all_tf_predictor_models
 
 if __name__ == "__main__":
+    osc = False
+    eval_tftype_classifier_model(osc=osc)
+    eval_all_tf_predictor_models(osc=osc)
+    
     osc = True
     eval_tftype_classifier_model(osc=osc)
     eval_all_tf_predictor_models(osc=osc)
--- backend/train_models.py
+++ backend/train_models.py
@@ -2,5 +2,10 @@ from src.tf_predictor.train_model import train_all
 from src.tftype_classifier.train_model import train as train_tftype_classifier
 
 if __name__=="__main__":
+    print("Training STEP models...")
     train_tftype_classifier(epochs=24, batch_size=256)
     train_all(epochs=60, batch_size=128)
+    
+    print("Training OSC models...")
+    train_tftype_classifier(epochs=24, batch_size=256, osc=True)
+    train_all(epochs=60, batch_size=128, osc=True)
